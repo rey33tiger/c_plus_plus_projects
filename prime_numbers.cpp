f5755c9922ceaa100d4c22970e6fecd17e871a22
rey33tiger <139722482+rey33tiger@users.noreply.github.com>
2023-07-17 17:17:38 -0400

    Create prime_numbers.cpp

---
+++ prime_numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream> 
+using namespace std;
+ 
+bool isPrime(int n)
+{
+      for(int i = 2; i < n/2; i++)
+      {
+        if(n % i == 0 or n <= 1) return false;
+      }
+      return true;
+}
+int main()
+{
+    int how_many = 1000;
+      for(int i = 1; i <= how_many; i++)
+      {
+          if(isPrime(i))
+          {
+          cout << i << "\n";
+          }
+    }
+    return 0;
+}
