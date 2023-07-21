0d10549d449496c3bef34b04f3a8b86176215e57
rey33tiger <139722482+rey33tiger@users.noreply.github.com>
2023-07-21 17:55:16 -0400

    Unbreakable calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,43 +1,53 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
 int main() {
-    double first_num;
-    double second_num;
-    char operand;
     bool x = true;
     while (x == true) {
+        std::string first_num;
+        std::string second_num;
+        char operand;
         std::cout << "Type First Number: ";
         std::cin >> first_num;
-        std::cout << "Type Operater(+, -, /, or *): ";
-        std::cin >> operand;
         std::cout << "Type Second Number: ";
         std::cin >> second_num;
-        switch (operand) {
-            case '+':
-                std::cout << "The answer is: " << first_num + second_num;
-                x = false;
+        for (int i = 0; i < first_num.size(); ++i) {
+            if (isdigit(first_num[i]) == false or isdigit(second_num[i]) == false) {
+                std::cout << "Invalid first number or Invalid second number \n\n";
                 break;
-            case '-':
-                std::cout << "The answer is: " << first_num - second_num;
-                x = false;
-                break;
-            case '/':
-                if (second_num == 0) {
-                    std::cout << "Can not divide by zero \n";
-                    break;
+            }
+            else {
+                std::cout << "Type Operater(+, -, /, or *): ";
+                std::cin >> operand;
+                double first_double = std::stod(first_num);
+                double second_double = std::stod(second_num);
+                switch (operand) {
+                    case '+':
+                        std::cout << "The answer is: " << first_double + second_double;
+                        x = false;
+                        break;
+                    case '-':
+                        std::cout << "The answer is: " << first_double - second_double;
+                        x = false;
+                        break;
+                    case '/':
+                        if (second_double == 0) {
+                            std::cout << "Can not divide by zero \n\n";
+                            break;
+                        }
+                        else {
+                            std::cout << "The answer is: " << first_double / second_double;
+                            x = false;
+                            break;
+                        }
+                    case '*':
+                        std::cout << "The answer is: " << first_double * second_double;
+                        x = false;
+                        break;
+                    default:
+                        std::cout  << "Type a valid operator \n\n";
+                        break;
                 }
-                else {
-                    std::cout << "The answer is: " << first_num / second_num;
-                    x = false;
-                    break;
-                }
-            case '*':
-                std::cout << "The answer is: " << first_num * second_num;
-                x = false;
-                break;
-            default:
-                std::cout  << "Type a valid operator \n\n";
-                break;
+            }
         }
     }
     return 0;
