f727f0571d1bfa0ed04f300b3ca551128ff31068
rey33tiger <139722482+rey33tiger@users.noreply.github.com>
2023-07-26 10:34:29 -0400

    Update calc.cpp
    except floats

--- calc.cpp
+++ calc.cpp
@@ -1,53 +1,61 @@
 // Online C++ compiler to run C++ program online
 #include <iostream>
+
+void input_output(std::string &first, std::string &second) {
+        std::cout << "Type First Number: ";
+        std::cin >> first;
+        std::cout << "Type Second Number: ";
+        std::cin >> second;
+}
 int main() {
+    std::string first_num;
+    std::string second_num;
+    char operand;
+    input_output(first_num, second_num);
     bool x = true;
     while (x == true) {
-        std::string first_num;
-        std::string second_num;
-        char operand;
-        std::cout << "Type First Number: ";
-        std::cin >> first_num;
-        std::cout << "Type Second Number: ";
-        std::cin >> second_num;
         for (int i = 0; i < first_num.size(); ++i) {
-            if (isdigit(first_num[i]) == false or isdigit(second_num[i]) == false) {
-                std::cout << "Invalid first number or Invalid second number \n\n";
+            if (isdigit(first_num[i]) == false) {
+                std::cout << "Invalid first number\n\n";
+                input_output(first_num, second_num);
+            }
+        }
+        for (int i = 0; i < second_num.size(); ++i) {
+            if (isdigit(second_num[i]) == false) {
+                std::cout << "Invalid second number\n\n";
+                input_output(first_num, second_num);
+            }
+        }
+        std::cout << "Type Operater(+, -, /, or *): ";
+        std::cin >> operand;
+        double first_double = std::stod(first_num);
+        double second_double = std::stod(second_num);
+        switch (operand) {
+            case '+':
+                std::cout << "The answer is: " << first_double + second_double;
+                x = false;
                 break;
-            }
-            else {
-                std::cout << "Type Operater(+, -, /, or *): ";
-                std::cin >> operand;
-                double first_double = std::stod(first_num);
-                double second_double = std::stod(second_num);
-                switch (operand) {
-                    case '+':
-                        std::cout << "The answer is: " << first_double + second_double;
-                        x = false;
-                        break;
-                    case '-':
-                        std::cout << "The answer is: " << first_double - second_double;
-                        x = false;
-                        break;
-                    case '/':
-                        if (second_double == 0) {
-                            std::cout << "Can not divide by zero \n\n";
-                            break;
-                        }
-                        else {
-                            std::cout << "The answer is: " << first_double / second_double;
-                            x = false;
-                            break;
-                        }
-                    case '*':
-                        std::cout << "The answer is: " << first_double * second_double;
-                        x = false;
-                        break;
-                    default:
-                        std::cout  << "Type a valid operator \n\n";
-                        break;
+            case '-':
+                std::cout << "The answer is: " << first_double - second_double;
+                x = false;
+                break;
+            case '/':
+                if (second_double == 0) {
+                    std::cout << "Can not divide by zero \n\n";
+                    break;
                 }
-            }
+                else {
+                    std::cout << "The answer is: " << first_double / second_double;
+                    x = false;
+                    break;
+                }
+            case '*':
+                std::cout << "The answer is: " << first_double * second_double;
+                x = false;
+                break;
+            default:
+                std::cout  << "Type a valid operator \n\n";
+                break;
         }
     }
     return 0;
